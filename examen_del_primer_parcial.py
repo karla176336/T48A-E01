595e3923f3a951167fcb26fc04e86002ccdc4a50
karla176336 <176336@upslp.edu.mx>
2025-09-11 19:30:40 -0600

    Update examen_del_primer_parcial.py

--- examen_del_primer_parcial.py
+++ examen_del_primer_parcial.py
@@ -5,3 +5,12 @@ import numpy as np
 nombre = "Karla Julieta Torres Elias"
 numero_de_matricula = 176336
 fecha = '2025-11-09'
+
+def capitalizacion():
+    datos = np.array([17, 21, 44, 50, 79, 86, 140, 178, 203])
+    media = np.mean(datos)
+    mediana = np.median(datos)
+    elementos_unicos, conteos = np.unique(datos, return_counts=True)
+    moda = elementos_unicos[np.argmax(conteos)]
+    desv_est = np.std(datos)
+    return (media, mediana, moda, desv_est)
