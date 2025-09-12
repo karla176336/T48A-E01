82986ccaa0d00bd37a36f4bf7d7932db158ba464
karla176336 <176336@upslp.edu.mx>
2025-09-11 19:14:15 -0600

    Update examen_del_primer_parcial.py

--- examen_del_primer_parcial.py
+++ examen_del_primer_parcial.py
@@ -1,1 +1,7 @@
+import numpy as np
 
+# Importante: verifica que tu nombre y número de matrícula esten correctos
+
+nombre = "Karla Julieta Torres Elias"
+numero_de_matricula = 176336
+fecha = '2025-11-09'
