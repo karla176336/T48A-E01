3f29d0e43b85cf46e5607ebce4d059507c005c92
karla176336 <176336@upslp.edu.mx>
2025-09-11 19:36:30 -0600

    Update examen_del_primer_parcial.py

--- examen_del_primer_parcial.py
+++ examen_del_primer_parcial.py
@@ -14,3 +14,18 @@ def capitalizacion():
     moda = elementos_unicos[np.argmax(conteos)]
     desv_est = np.std(datos)
     return (media, mediana, moda, desv_est)
+
+def asistencia_dispersion():
+    """
+    La aistencia a los 10 últimos partidos en casa de las Águilas de Baltimore fue la siguiente:
+
+    [20100, 24500, 31600, 28400, 49500, 19350, 25600, 30600, 11300, 28560]
+
+    Calcule el rango, la varianza y la desviación stándard para estos datos
+    Regrese una tupla con el siguiente orden, como se muestra a continuación:
+    """
+    datos = np.array([20100, 24500, 31600, 28400, 49500, 19350, 25600, 30600, 11300, 28560])
+    rango = np.ptp(datos) 
+    varianza = np.var(datos)
+    desv_est = np.std(datos)
+    return (rango, varianza, desv_est)
